801c5e8a0e8d3fc437779c59e266ce38e0e71965
dffccce234 <dffccce234@proton.me>
2025-04-24 14:55:18 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -12,8 +12,8 @@ export ZLIB_HASH="9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df
 export LIBEVENT_VERSION="2.1.12-stable"
 export LIBEVENT_HASH=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
 
-export OPENSSL_VERSION="3.4.0"
-export OPENSSL_HASH="e15dda82fe2fe8139dc2ac21a36d4ca01d5313c75f99f46c4e8a27709b7294bf"
+export OPENSSL_VERSION="3.5.0"
+export OPENSSL_HASH="344d0a79f1a9b08029b0744e2cc401a43f9c90acd1044d09a530b4885a8e9fc0"
 
 export DOCKER="$(command -v docker || command -v podman)"
 
